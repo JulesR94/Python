7b5f8e8d4484b660113dd115a7ef0fdba4dead66
JulesR94 <96482907+JulesR94@users.noreply.github.com>
2022-01-10 13:20:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-a =7
+a=7
 print(a+3)
+b=3
+print(a+b) #afficher le resultat des valeurs respectives des variables a et b
+print(a)
+#a = b + 1
+#print(a)
+#print(a+b)
+#a += b # a = a + b
+print(a)
+
+a, b = 11, 4
+c =a//b
+d = a%b
+print('la division de '+ str(a) + ' par ' + str(b) + ' est égal à ' + str(c) + '.Il reste ' + str(d))
+print(d)
+
+chaine = "Et voilà du texte"
+chaine1= 'Nous l\'avons '
+chaine2= "\"réparée\""
+print(chaine1 + chaine2)
