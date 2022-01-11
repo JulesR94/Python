8cce35eecbc8bca137c9fd42df2b9e5c7f5097d0
JulesR94 <96482907+JulesR94@users.noreply.github.com>
2022-01-11 08:50:10 +0100

    change

--- test.py
+++ test.py
@@ -8,14 +8,38 @@ print(a)
 #print(a+b)
 #a += b # a = a + b
 print(a)
-
 a, b = 11, 4
-c =a//b
-d = a%b
+c =a//b # // = Pour afficher un nombre entier
+d = a%b #%=modulo-->
 print('la division de '+ str(a) + ' par ' + str(b) + ' est égal à ' + str(c) + '.Il reste ' + str(d))
 print(d)
 
 chaine = "Et voilà du texte"
 chaine1= 'Nous l\'avons '
 chaine2= "\"réparée\""
-print(chaine1 + chaine2)
+print(chaine1 + " " +chaine2)
+
+#reponse = input() # une ligne vide apparaît et attend que l'utilisateur rentre une information
+age = int (input("Quel est votre âge ?"))
+print("Vous avez " + str (age) + " ans")
+
+if  age > 16 and age < 100 : 
+    print("Vous avez plus de 16 ans")
+elif age > 100 :
+    print("Tu te moquerais pas de moi?")    
+else :
+    print("Tu es encore un peu jeune :(")
+
+a=3
+print(type(a))
+b=8.2
+print(type(b))
+chaine= "Au revoir"
+print (type(chaine))
+vrai = False
+print(type(vrai))
+
+print (7 > 5 > 1)
+
+print (7 > 5 < 9 !=10)
+
---
+++ test1.py
@@ -0,0 +1,8 @@
+i = 0
+while i < 10: #tant que
+    i += 1
+    if i%2 == 0: # Si i est impair / == : égale dans une condition / !=: différent /
+        continue    
+    print(i) 
+    
+
